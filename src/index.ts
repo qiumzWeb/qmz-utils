e31ac27a1b8e22fa2505a269d5db7c0979d72b7b
qmz01080910 <qmz01080910@cainiao.com>
2021-07-12 16:20:40 +0800

    新增indexDB 工具

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ import Ajax from './lib/ajax'
 import Cookie from './lib/cookie'
 import AsyncAntShake from './lib/asyncAntiShake'
 import LocalStore from './lib/localStore'
-
+import IndexDBStore from './lib/indexDBStore'
 export {
     Ajax,
     Cookie,
     AsyncAntShake,
-    LocalStore
+    LocalStore,
+    IndexDBStore
 }
