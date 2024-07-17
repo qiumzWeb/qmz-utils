41b87d713cb8eabd7da85af6e57f23c39a1f7a93
qiumzWeb <qiumzjob@qq.com>
2024-07-17 18:18:32 +0800

    ddd

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,4 @@
 export * from './lib/tools';
-export { default as Ajax } from './lib/ajax';
 export { default as Cookie } from './lib/cookie';
 export { default as AsyncAntShake } from './lib/asyncAntiShake';
 export { default as LocalStore } from './lib/localStore';
